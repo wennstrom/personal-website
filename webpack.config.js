c61928831919d78518d7c986a1a5404c4bdd6334
Oskar <oskar.wennstrom@gmail.com>
2022-05-20 23:01:07 +0200

    added react support

--- webpack.config.js
+++ webpack.config.js
@@ -56,13 +56,13 @@ Encore
     })
 
     // enables Sass/SCSS support
-    //.enableSassLoader()
+    enableSassLoader()
 
     // uncomment if you use TypeScript
-    //.enableTypeScriptLoader()
+    .enableTypeScriptLoader()
 
     // uncomment if you use React
-    //.enableReactPreset()
+    enableReactPreset()
 
     // uncomment to get integrity="..." attributes on your script & link tags
     // requires WebpackEncoreBundle 1.4 or higher
